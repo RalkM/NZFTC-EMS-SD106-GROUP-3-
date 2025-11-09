8efcd6694538cdd3a62cdfc9791c67bced6ebf8d
ars1224 <126647608+ars1224@users.noreply.github.com>
2025-11-09 23:52:13 +1300

    trying to make the support(grievances)

--- NZFTC-EMS/obj/Debug/net9.0/NZFTC-EMS.AssemblyInfo.cs
+++ NZFTC-EMS/obj/Debug/net9.0/NZFTC-EMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZFTC-EMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60f76921478e6407d12bbc755c311ba9dd8e0459")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c33298e79c3a3052e14072356bc002c58d78ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZFTC-EMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZFTC-EMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

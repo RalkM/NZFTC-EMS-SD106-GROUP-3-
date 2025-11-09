0e748e922ff93a60b58ebbc9d329923bb21e3560
RalkM <kiritomunsayac@gmail.com>
2025-11-10 07:33:48 +1300

    Made some changes to Program.cs

--- NZFTC-EMS/Program.cs
+++ NZFTC-EMS/Program.cs
@@ -1,35 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZFTC_EMS.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("NZFTC_EMS_TestDB"));
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 builder.Services.AddControllersWithViews();
-builder.Services.AddSession(); // <-- add this
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/website/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Website/Error");
     app.UseHsts();
 }
-
-
-
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseSession(); // <-- and this
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=website}/{action=Index}/{id?}")
-    .WithStaticAssets();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+
+
+app.UseSession();
+
+
+app.Use(async (context, next) =>
+{
+    
+    if (string.IsNullOrEmpty(context.Session.GetString("Role")))
+    {
+        context.Session.SetString("Role", "Employee");   
+        context.Session.SetString("Username", "John Doe");
+    }
+    await next();
+});
+
 app.UseAuthorization();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Website}/{action=Login}/{id?}"
+);
 
 app.Run();

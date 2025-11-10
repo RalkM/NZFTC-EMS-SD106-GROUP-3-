039384445ce60b59e91754ac6fa7989dce6c0b9f
ars1224 <126647608+ars1224@users.noreply.github.com>
2025-11-10 18:45:40 +1300

    extra

--- NZFTC-EMS/obj/Debug/net9.0/NZFTC-EMS.AssemblyInfo.cs
+++ NZFTC-EMS/obj/Debug/net9.0/NZFTC-EMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZFTC-EMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8efcd6694538cdd3a62cdfc9791c67bced6ebf8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f012d0a985a44452ef828da714f09a2ce21ba1eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZFTC-EMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZFTC-EMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

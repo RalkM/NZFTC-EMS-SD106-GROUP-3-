37548c2bec820e62a95d507091acb46c86033006
ars1224 <126647608+ars1224@users.noreply.github.com>
2025-11-18 21:55:23 +1300

    merge

--- NZFTC-EMS/obj/Debug/net9.0/NZFTC-EMS.AssemblyInfo.cs
+++ NZFTC-EMS/obj/Debug/net9.0/NZFTC-EMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZFTC-EMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+477a276d073bbcca3079a3fd828078ef9d6d16f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+250bc22a90654c16ca63ea286614d9ca7ea9255c")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZFTC-EMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZFTC-EMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
